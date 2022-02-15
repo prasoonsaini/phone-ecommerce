7e5b7b5842a51f8861f7aa3003e812cf23bd903f
prasoonsaini <prasoonsaini2002@gmail.com>
2022-02-15 22:06:19 +0530

    Updates

---
+++ precache-manifest.127f781b3c34b2b17eadfc5c6bdc52ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "79fbe2476cc844d9cc3c",
+    "url": "/phone-ecommerce/static/css/main.604a4908.chunk.css"
+  },
+  {
+    "revision": "79fbe2476cc844d9cc3c",
+    "url": "/phone-ecommerce/static/js/main.79fbe247.chunk.js"
+  },
+  {
+    "revision": "6feb97c8fcc5d5628a30",
+    "url": "/phone-ecommerce/static/css/1.3c9bdf99.chunk.css"
+  },
+  {
+    "revision": "6feb97c8fcc5d5628a30",
+    "url": "/phone-ecommerce/static/js/1.6feb97c8.chunk.js"
+  },
+  {
+    "revision": "838a7409eff7351a3e43",
+    "url": "/phone-ecommerce/static/js/runtime~main.838a7409.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/phone-ecommerce/static/media/logo.9f7e506d.svg"
+  },
+  {
+    "revision": "295fe45e1ab5d8f06a1833fd4a6e3c69",
+    "url": "/phone-ecommerce/index.html"
+  }
+];
